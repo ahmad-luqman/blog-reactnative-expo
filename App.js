605575ae45989be2d7748578e837f6a93550c123
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-14 19:58:53 +0500

    add stack navigator

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Import screens
+import IndexScreen from "./src/screens/IndexScreen";
+// import ShowScreen from "./src/screens/ShowScreen";
+// import CreateScreen from "./src/screens/CreateScreen";
+// import EditScreen from "./src/screens/EditScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// Create a stack navigator
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+    // Show: ShowScreen,
+    // Create: CreateScreen,
+    // Edit: EditScreen,
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
+  }
+);
+
+// Create an app container
+export default createAppContainer(navigator);
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button title="Add" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
