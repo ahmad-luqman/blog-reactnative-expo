7949a2d372818c9b20d3d576306e5856d2296246
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-16 13:00:58 +0500

    Edit Icon linking

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -11,11 +12,38 @@ const ShowScreen = ({ navigation }) => {
   
   return (
     <View>
-      <Text>{blogPosts.title}</Text>
+      <Text style={styles.title}>{blogPosts.title}</Text>
+      <Text style={styles.content}>{blogPosts.content}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <EvilIcons
+        name="pencil"
+        size={35}
+        onPress={() =>
+          navigation.navigate("Edit", { id: navigation.getParam("id") })
+        }
+      />
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  content: {
+    fontSize: 16,
+    textAlign: "center",
+    marginHorizontal: 10,
+  },
+});
 
 export default ShowScreen;
