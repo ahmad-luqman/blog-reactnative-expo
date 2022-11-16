6b6a1e1dbe3dab56d3d4d6ad6bd413574f196824
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-16 15:08:35 +0500

    add EditScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
 
   const blogPost = state.find(
-    (blogPost) => blogPost.id === navigation.getParam('id')
+    (blogPost) => blogPost.id === id
   );
 
   return (
-    <View>
-      <Text>Edit Screen - {blogPost.id}</Text>
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
   );
-}
-
-const styles = StyleSheet.create({});
+};
 
 export default EditScreen;
